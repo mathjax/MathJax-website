49927f088c919fcfa394fca48e0f897ede4bbb67
Peter Krautzberger <p.krautzberger@gmail.com>
2015-01-26 21:54:41 +0100

    for #12: make JS pausing work for youtube embed

--- js/frontpage.js
+++ js/frontpage.js
@@ -11,9 +11,10 @@ var modals = ['modal-samples','modal-livedemo','modal-zoom','modal-sponsorship']
 if (modals.indexOf(hash.substring(1,hash.length)) > -1) {
    $(hash).modal('show');
 } else {}
-var video = document.getElementsByTagName('video')[0];
+var video = document.getElementsByTagName('iframe')[0];
 $('#modal-zoom').on('hidden.bs.modal', function () {
-  video.pause();
+//  video.pause(); // for <video> elements
+  video.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
 });
 });
 

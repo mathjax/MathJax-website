d85e38b16f39e31ffe9a4b804aa2fcd0df58a44d
Davide P. Cervone <dpvc@union.edu>
2020-02-05 15:20:43 -0500

    Add id to script that loads MathJax

--- js/main.js
+++ js/main.js
@@ -119,4 +119,5 @@ window.onhashchange = followHash;
 //
 var script = document.createElement('script');
 script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+script.id = 'MathJax-sxcript';
 document.head.appendChild(script);

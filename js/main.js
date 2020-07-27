70e471776b42a6e9c3385770d9bd1c3c1e7ef54c
Davide P. Cervone <dpvc@union.edu>
2020-07-27 16:33:17 -0400

    Update main.js and CORS headers

--- js/main.js
+++ js/main.js
@@ -21,39 +21,42 @@ window.MathJax = {
       //
       // Synchronize menu renderer item with on-screen popup menu
       //
-      var renderer = MathJax.startup.document.menu.settings.renderer;
       var select = document.getElementById('Renderer');
-      var item = MathJax.startup.document.menu.menu.getPool().lookup('renderer');
-      select.value = renderer;
-      if (renderer !== 'CHTML') item.setValue(renderer);
-      item.registerCallback(function () {
-        var value = item.getValue();
-        if (value !== select.value) select.value = value;
-      });
-      window.setMode = function (renderer) {
-        if (item.getValue() !== renderer) item.setValue(renderer);
-      }
-      //
-      //  Set up processing of input content
-      //
-      var input = document.getElementById('MathInput');
-      var output = document.getElementById('MathPreview');
-      var button = document.getElementById('renderHTML');
-      output.innerHTML = input.value.trim();
-      window.typesetInput = function () {
-        button.disabled = true;
-        output.innerHTML = input.value.trim();
-        MathJax.texReset();
-        MathJax.typesetClear();
-        MathJax.typesetPromise([output]).catch(function (err) {
-          output.innerHTML = '';
-          output.appendChild(document.createTextNode(err.message));
-          console.error(err);
-        }).then(function () {
-          button.disabled = false;
+      if (select) {
+        var renderer = MathJax.startup.document.menu.settings.renderer;
+        var menu = MathJax.startup.document.menu.menu;
+        var item = (menu.getPool ? menu.getPool() : menu.pool).lookup('renderer');
+        select.value = renderer;
+        if (renderer !== 'CHTML') item.setValue(renderer);
+        item.registerCallback(function () {
+          var value = item.getValue();
+          if (value !== select.value) select.value = value;
         });
+        window.setMode = function (renderer) {
+          if (item.getValue() !== renderer) item.setValue(renderer);
+        }
+        //
+        //  Set up processing of input content
+        //
+        var input = document.getElementById('MathInput');
+        var output = document.getElementById('MathPreview');
+        var button = document.getElementById('renderHTML');
+        output.innerHTML = input.value.trim();
+        window.typesetInput = function () {
+          button.disabled = true;
+          output.innerHTML = input.value.trim();
+          MathJax.texReset();
+          MathJax.typesetClear();
+          MathJax.typesetPromise([output]).catch(function (err) {
+            output.innerHTML = '';
+            output.appendChild(document.createTextNode(err.message));
+            console.error(err);
+          }).then(function () {
+            button.disabled = false;
+          });
+        }
+        input.oninput = typesetInput;
       }
-      input.oninput = typesetInput;
 
       return MathJax.startup.defaultPageReady();
     }
